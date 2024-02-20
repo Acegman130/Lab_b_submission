0c00675cbff22facd05e3eeb11fe011b11c2ef2a
Gabriel Harris <gabriel.harris-2@student.manchester.ac.uk>
2024-02-20 04:25:08 +0000

    Finished lab b task

---
+++ task_b.c
@@ -0,0 +1,20 @@
+#define _USE_MATH_DEFINES
+#include <stdio.h>
+#include <math.h>
+int main(void){
+    double height = 0;
+	double volume = 0;
+	printf("Enter height: ");
+	scanf("%lf", &height);
+	
+	
+	// Your code below here
+	double radius = 0; //creates the variable radius
+	radius = 2.2; // sets the radius to constant 2.2
+	volume = height*(M_PI * pow(radius, 2)); // calculate the volume of cylinder 
+	
+	
+	// Do not edit below here
+	printf("Volume: %.2f", volume);
+	return 0;
+}
